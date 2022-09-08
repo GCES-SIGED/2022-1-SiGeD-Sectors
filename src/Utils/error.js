300302cd8f92e01415aa09f87512c5848fc5a0bf
giovannabbottino <giovannabbottino@gmail.com>
2022-09-08 20:12:03 -0300

    add error file

---
+++ src/Utils/error.js
@@ -0,0 +1,9 @@
+function newError(name) {
+    new Error(name);
+}
+
+function throwAggregateError(errors, message) {
+    throw new AggregateError(errors, message);
+}
+
+module.exports = { newError, throwAggregateError };
